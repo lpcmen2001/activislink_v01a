d63647be2e74af35be722c15627f961d2859577b
couturelp <lpcmen2001@gmail.com>
2017-09-05 19:21:36 +0800

    passwords

--- activislink/imports/ui/Signup.js
+++ activislink/imports/ui/Signup.js
@@ -1,14 +1,38 @@
 import React  from 'react';
 import { Link } from 'react-router-dom';
+import {Accounts} from 'meteor/accounts-base'
 
 export default class Signup extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        error: '' 
+      };
+  }
+  onSubmit(e){
+      e.preventDefault();
+      
+      let email = this.refs.email.value.trim();
+      let password = this.refs.password.value.trim();
+
+      Accounts.createUser({email: email, password: password},(err)=>{
+        console.log("Signup callback", err);
+      });
+  }
   render(){
     return (
       <div>
         <h1>Join ActivisLink</h1>
 
-        <p>Signup form here</p>
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
+
+        <p>We are currently in open signups.</p>
         
+        <form onSubmit={this.onSubmit.bind(this)}>
+            <input type="email" ref="email" name="email" placeholder="Email"/>
+            <input type="password" ref="password" name="password" placeholder="Password"/>
+            <button>Create Account</button>
+        </form>
         <Link to="/">Already have an account ?</Link>
       </div>
     );
